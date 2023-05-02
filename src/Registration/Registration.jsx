74abdeb1150373c382a112569b544dccaed98b42
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2023-05-02 15:03:09 +0600

    registration success

--- src/Registration/Registration.jsx
+++ src/Registration/Registration.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Providers/AuthProviders';
 
 const Registration = () => {
 
+    const [error, setError] = useState(null);
+    const { createUser } = useContext(AuthContext);
 
-    const handleRegister = () => {
-
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.text.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const img = form.imgurl.value;
+        // console.log(name, email, password, img);
+        setError(null);
+        createUser(email, password)
+            .then(res => {
+                const loggedUser = res.user;
+                console.log(loggedUser);
+            })
+            .catch(e => {
+                setError(e.message);
+            })
     }
     return (
         <div>
@@ -17,20 +35,26 @@ const Registration = () => {
                             <label className="label">
                                 <span className="text-base label-text">Name</span>
                             </label>
-                            <input type="text" name='text' placeholder="Your Name" className="w-full input input-bordered input-primary" />
+                            <input type="text" name='text' placeholder="Your Name" className="w-full input input-bordered input-primary" required />
                         </div>
                         <div>
                             <label className="label">
                                 <span className="text-base label-text">Email</span>
                             </label>
-                            <input type="text" name='text' placeholder="Email Address" className="w-full input input-bordered input-primary" />
+                            <input type="text" name='email' placeholder="Email Address" className="w-full input input-bordered input-primary" required />
                         </div>
                         <div>
                             <label className="label">
                                 <span className="text-base label-text">Password</span>
                             </label>
                             <input type="password" placeholder="Enter Password" name='password'
-                                className="w-full input input-bordered input-primary" />
+                                className="w-full input input-bordered input-primary" required />
+                        </div>
+                        <div>
+                            <label className="label">
+                                <span className="text-base label-text">Image Url</span>
+                            </label>
+                            <input type="text" name='imgurl' placeholder="Your Name" className="w-full input input-bordered input-primary" />
                         </div>
                         <p className="text-xs text-gray-600 hover:underline hover:text-blue-600">Already have an account on  chef koi? please  <Link to='/login'>Login</Link> </p>
                         <div>
